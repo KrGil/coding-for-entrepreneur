15859c7edb772dd7a630f178402022590a7f71fe
KrGil <Gil@Changui-MacBook-Air.local>
2017-06-26 16:34:44 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Gil on 2017. 6. 26..
+ */
+public class Main {
+    public static void main(String[] args){
+        // System.out.println("hello");
+
+        // operators
+        Operators operators = new Operators();
+        operators.calculat();
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Gil on 2017. 6. 26..
+ */
+public class Operators {
+    public void calculat() {
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(5 * 4);
+        System.out.println(6 / 2);
+
+//     special case
+        System.out.println(5 / 2);
+        System.out.println(5 % 2);
+    }
+}
