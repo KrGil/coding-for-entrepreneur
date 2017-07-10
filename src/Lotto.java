398da31f07a33b79b5bd5394fa4ea8f14f852fec
KrGil <Gil@Changui-MacBook-Air.local>
2017-07-10 15:15:20 +0900

    perfect

--- src/Lotto.java
+++ src/Lotto.java
@@ -22,12 +22,13 @@ public class Lotto {
         HashSet<Integer> hashSet = new HashSet<>();
 
         // 2. hashSet의 전체 갯수가 6일 때만 작동하는 while 문을 만든다
-        while (hashSet.size() == 6) {
+        // while (조건) ()안의 조건이 참이 아니면 작동하지 않는다
+        while (hashSet.size() < 6) {
             // 3. while문 안에서 난수를 만들어서 hashSet 안에 집어넣는다
             int randomNum = 1 + (int) (Math.random() * ((24) + 1));
             hashSet.add(randomNum);
             // 4. hashSet 안의 값을 출력한다
-            System.out.println(hashSet.size());
         }
+        System.out.println(hashSet);
     }
 }
